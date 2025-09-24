1dbdc9a08584d1e7edf0edccd11021a7cbfac440
Kavya  Amballa <amballakavya11@gmail.com>
2025-09-24 20:42:07 +0530

    Add files via upload

---
+++ DAY 1 Intro.sql
@@ -0,0 +1,17 @@
+/*What is a Database?
+A collection of data organized for easy access and management.*/
+
+/*Types of Databases:
+Relational Databases (SQL-based, e.g., MySQL, PostgreSQL).
+Non-Relational Databases (NoSQL, e.g., MongoDB).*/
+
+-- SQL keywords are NOT case sensitive: select is the same as SELECT
+
+-- The SELECT statement is used to select data from a database.
+/*Syntax 
+Select * from table_name  (retrieves entire data)
+select col_name ,col_name from table_name  (retrieves specified columns data)*/
+
+
+SELECT 'Hello , SQL!!';
+
